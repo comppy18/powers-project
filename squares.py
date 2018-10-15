eeb0a2d38b6c17b8aba13ba3ec9a485b37541ee5
Olav Vahtras <vahtras@kth.se>
2018-10-15 15:07:17 +0200

    Introduce function

--- squares.py
+++ squares.py
@@ -1,2 +1,9 @@
-for i in (1, 2, 3, 4):
-    print(i**2)
+def list_squares(n):
+    "returns a list of squares"
+    li = [] 
+    for i in range(1, n+1):
+        li.append(i**2)
+    return li
+
+for i in list_squares(4):
+    print(i)
