368cdf83d3067edb09b8530aca51e721456e2cd3
Olav Vahtras <vahtras@kth.se>
2018-10-15 15:20:44 +0200

    Generalize from squares to powers

--- squares.py
+++ squares.py
@@ -1,9 +1,9 @@
-def list_squares(n):
+def list_powers(n, m):
     "returns a list of squares"
     li = [] 
     for i in range(1, n+1):
-        li.append(i**2)
+        li.append(i**m)
     return li
 
-for i in list_squares(4):
+for i in list_powers(4, 3):
     print(i)
